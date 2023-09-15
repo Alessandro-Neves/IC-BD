8bfb3526ec491361a26ea6574d4f902c99c34fc8
alessandro <git.aleneves@gmail.com>
2023-09-15 11:02:54 -0300

    15.09.23: implement duck dc detector distinct

--- denial-constraint-violations/v0.0.2/dcd/duck/dc_detector_sql.py
+++ denial-constraint-violations/v0.0.2/dcd/duck/dc_detector_sql.py
@@ -26,49 +26,46 @@ class DCDetector(IDCDetector):
     return pairs
   
   def __dc_predicates_to_SQL_query(self, dc: IDC):
+    # return """
+    #   SELECT DISTINCT t1.ID, t2.ID FROM T AS t1, T AS t2 WHERE t1.salary > t2.salary AND t1.hiring_year > t2.hiring_year AND t1.id <> t2.id;
+    # """
+    
     scalar_predicates = list(filter(lambda p: not p.is_relational, dc.get_predicates()))
     relational_predicates = [p for p in dc.get_predicates() if p not in scalar_predicates]
-    
-    used_ON_clause = False
-    used_WHERE_clause = False
     
     same_targets_rps = list(filter(lambda p: not p.has_diff_target, relational_predicates))
     diff_targets_rps = [p for p in relational_predicates if p not in same_targets_rps]
     
-    sql_query = "SELECT t1.id as id1, t2.id as id2 FROM T t1 JOIN T t2"
-    
-    if not bool(diff_targets_rps):
-      sql_query += " ON t1.id = t2.id"
+    sql_query = "SELECT DISTINCT t1.id as id1, t2.id as id2 FROM T AS t1, T AS t2 WHERE"
 
+    use_AND_clause = False
+    
     if bool(diff_targets_rps):
       for rps in diff_targets_rps:
-        if not used_ON_clause:
-          sql_query += f" ON t1.{rps.left_side.col_name_or_value} {rps.operator.value} t2.{rps.right_side.col_name_or_value}"
-          used_ON_clause = True
-        else:
-          sql_query += f" AND t1.{rps.left_side.col_name_or_value} {rps.operator.value} t2.{rps.right_side.col_name_or_value}"
+        if use_AND_clause:
+          sql_query += " AND"
+        use_AND_clause = True
+        sql_query += f" t1.{rps.left_side.col_name_or_value} {rps.operator.value} t2.{rps.right_side.col_name_or_value}"
 
     if bool(same_targets_rps):
       for rps in same_targets_rps:
-        if not used_WHERE_clause:
-          sql_query += f" WHERE t1.{rps.left_side.col_name_or_value} {rps.operator.value} t1.{rps.right_side.col_name_or_value}"
-          used_WHERE_clause = True
-        else:
-          sql_query += f" AND t1.{rps.left_side.col_name_or_value} {rps.operator.value} t1.{rps.right_side.col_name_or_value}"
+        if use_AND_clause:
+          sql_query += " AND"
+        use_AND_clause = True
+        sql_query += f" t1.{rps.left_side.col_name_or_value} {rps.operator.value} t1.{rps.right_side.col_name_or_value}"
 
     if bool(scalar_predicates):
       for sps in scalar_predicates:
-        if not used_WHERE_clause:
-          sql_query += f" WHERE t1.{sps.left_side.col_name_or_value} {sps.operator.value} {sps.right_side.col_name_or_value}"
-          used_WHERE_clause = True
-        else:
-          sql_query += f" AND t1.{sps.left_side.col_name_or_value} {sps.operator.value} {sps.right_side.col_name_or_value}"
-
-    if not bool(scalar_predicates) and not bool(same_targets_rps):
-          sql_query += " AND t1.id <> t2.id"
+        if use_AND_clause:
+          sql_query += " AND"
+        use_AND_clause = True
+        sql_query += f" t1.{sps.left_side.col_name_or_value} {sps.operator.value} {sps.right_side.col_name_or_value}"
+    
+    sql_query += " AND t1.id <> t2.id"
 
     sql_query += ";"
 
     print(sql_query)
 
     return sql_query
+    
--- denial-constraint-violations/v0.0.2/run.py
+++ denial-constraint-violations/v0.0.2/run.py
@@ -1,87 +1,41 @@
 import time
-
-from dcd.core.dc import DC 
 from dcd.core.session import Session 
 from dcd.core.dc_reader import DCReader
 from dcd.core.dc_detector import DCDetector
 from dcd.duck.dc_detector import DCDetector as DuckDCDetector
+from dcd.duck.dc_detector_sql import DCDetector as DuckDistinticDCDetector
 from dcd.polars.dc_detector import DCDetector as PolarsDCDetector
 
 # exit(0)
 NOISY = True
-ALIAS = '40k'
+ALIAS = '10k'
 
 DC_FILE = 'testdatas/employees-dc.txt'
 DATASET_FILE = f'testdatas/employees-{ALIAS}-noisy.csv' if NOISY else f'testdatas/employees-{ALIAS}.csv'
 
-print("Pandas:")
-dc_detector = DCDetector()
 
-dc_reader = DCReader(DC_FILE)
-session = Session(DATASET_FILE, dc_reader, dc_detector)
+detectors = [
+  DCDetector(),
+  PolarsDCDetector(),
+  DuckDCDetector(),
+  DuckDistinticDCDetector()
+]
 
-cur_time = time.monotonic()
-session.detect_violations()
-end_time = time.monotonic()
+names = ["Pandas", "Polars", "Duck JOIN", "Duck DISTINCT"]
 
-violations = session.get_violations()
-
-print(len(violations))
-print(end_time - cur_time)
-print(violations)
-print()
-
-
-print("DuckDB:")
-duck_dc_detector = DuckDCDetector()
-
-dc_reader = DCReader(DC_FILE)
-session = Session(DATASET_FILE, dc_reader, duck_dc_detector)
-
-cur_time = time.monotonic()
-session.detect_violations()
-end_time = time.monotonic()
-
-violations = session.get_violations()
-
-print(len(violations))
-print(end_time - cur_time)
-# print(violations)
-print()
-
-
-# clean_data = session.get_clean_cells()
-# noisy_data = session.get_noisy_cells()
-
-# # OUTPUT
-
-# print(session.data,  end='\n\n')
-
-
-
-# print('clean cells:')
-# print(clean_data, end='\n\n')
-# print('noisy cells:')
-# print(noisy_data, end='\n\n')
-
-print("Polars:")
-polars_dc_detector = PolarsDCDetector()
-
-dc_reader = DCReader(DC_FILE)
-session = Session(DATASET_FILE, dc_reader, polars_dc_detector)
-
-cur_time = time.monotonic()
-session.detect_violations()
-end_time = time.monotonic()
-
-violations = session.get_violations()
-
-print(len(violations))
-print(end_time - cur_time)
-# print(violations)
-print()
-
-
-# for v in violations:
-#   print(v)
-# print('\n')
+for i, detector in enumerate(detectors):
+  print(f"{names[i]}: ")
+  dc_detector = detector
+  dc_reader = DCReader(DC_FILE)
+  session = Session(DATASET_FILE, dc_reader, dc_detector)
+  
+  cur_time = time.monotonic()
+  session.detect_violations()
+  end_time = time.monotonic()
+  
+  violations = session.get_violations()
+  
+  # print(len(violations))
+  print(end_time - cur_time)
+  # print(violations)
+  print()
